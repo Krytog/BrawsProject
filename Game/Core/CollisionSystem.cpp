cc23418b73922fd93c7c5f2f9e980a4d84582b69
Daniil Baranov <danya2003.db@gmail.com>
2023-01-09 04:46:41 +0300

    Add CollisionSystem

---
+++ Game/Core/CollisionSystem.cpp
@@ -0,0 +1,42 @@
+#include "CollisionSystem.h"
+
+CollisionSystem::CollisionInfo::CollisionInfo(const GameObject *game_object_, const std::string_view tag_,
+                                              const Position &position_): game_object(game_object_), tag(tag_), position(position_) {}
+
+bool CollisionSystem::IsRegistered(const GameObject *game_object) const {
+    return registered_colliders_.contains(game_object);
+}
+
+const Collider *CollisionSystem::GetRegisteredColliderOf(const GameObject *game_object) const {
+    return registered_colliders_.at(game_object);
+}
+
+void CollisionSystem::UnregisterColliderOf(const GameObject *game_object) {
+    if (registered_colliders_.contains(game_object)) {
+        registered_colliders_.erase(game_object);
+    }
+}
+
+void CollisionSystem::RegisterColliderOf(const GameObject *game_object, const Collider *collider) {
+    registered_colliders_[game_object] = collider;
+}
+
+std::optional<Position> CollisionSystem::CheckCollision(const GameObject *first, const GameObject *second) const {
+    if (!registered_colliders_.contains(first) || !registered_colliders_.contains(second)) {
+        return std::nullopt;
+    }
+    return registered_colliders_.at(first)->GetIntersectionPosition(registered_colliders_.at(second));
+}
+
+std::vector<CollisionSystem::CollisionInfo> CollisionSystem::GetAllCollisions(const GameObject *game_object) const {
+    if (!registered_colliders_.contains(game_object)) {
+        return {};
+    }
+    std::vector<CollisionInfo> output;
+    for (const auto& pair : registered_colliders_) {
+        if (auto position = registered_colliders_.at(game_object)->GetIntersectionPosition(pair.second)) {
+            output.emplace_back(pair.first, pair.first->GetTag(), position.value());
+        }
+    }
+    return output;
+}
