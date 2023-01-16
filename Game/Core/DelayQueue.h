92a1bd48cc5541abb457d7f26e7f2275500d2176
Daniil Baranov <danya2003.db@gmail.com>
2023-01-17 00:57:39 +0300

    Add complete implementation of DelayQueue

--- Game/Core/DelayQueue.h
+++ Game/Core/DelayQueue.h
@@ -16,9 +16,29 @@ public:
         queue_time_.Push(time_point, func, args...);
     }
 
+    template<typename F, typename... Args>
+    void PushTime(const TimePoint& time_point, F* pointer, void(F::*func)(Args...), Args... args) {
+        queue_time_.Push(time_point, pointer, func, args...);
+    }
+
+    template<typename F, typename... Args>
+    void PushTime(const TimePoint& time_point, const F* pointer, void(F::*func)(Args...) const, Args... args) {
+        queue_time_.Push(time_point, pointer, func, args...);
+    }
+
     template<typename... Args>
     void PushTicks(const uint64_t ticks_count, void(*func)(Args...), Args... args) {
         queue_ticks_.Push(ticks_count, func, args...);
+    }
+
+    template<typename F, typename... Args>
+    void PushTicks(const uint64_t ticks_count, F* pointer, void(F::*func)(Args...), Args... args) {
+        queue_ticks_.Push(ticks_count, pointer, func, args...);
+    }
+
+    template<typename F, typename... Args>
+    void PushTicks(const uint64_t ticks_count, const F* pointer, void(F::*func)(Args...) const, Args... args) {
+        queue_ticks_.Push(ticks_count, pointer, func, args...);
     }
 
     void TryExecute(const TimePoint& time_point, const uint64_t ticks_count);
@@ -37,6 +57,17 @@ private:
         void Push(const TimePoint& time_point, void(*func)(Args...), Args... args) {
             queue_.emplace(time_point, [func, args...](){func(args...);});
         }
+
+        template<typename F, typename... Args>
+        void Push(const TimePoint& time_point, F* pointer, void(F::*func)(Args...), Args... args) {
+            queue_.emplace(time_point, [func, pointer, args...](){(pointer->*func)(args...);});
+        }
+
+        template<typename F, typename... Args>
+        void Push(const TimePoint& time_point, const F* pointer, void(F::*func)(Args...) const, Args... args) {
+            queue_.emplace(time_point, [func, pointer, args...](){(pointer->*func)(args...);});
+        }
+
         void TryExecute(const TimePoint& time_point);
     private:
         std::priority_queue<TypeQueueTime, std::vector<TypeQueueTime>, CompareFunctor> queue_;
@@ -52,6 +83,16 @@ private:
         template<typename... Args>
         void Push(const uint64_t ticks_count, void(*func)(Args...), Args... args) {
             queue_.emplace(ticks_count, [func, args...](){func(args...);});
+        }
+
+        template<typename F, typename... Args>
+        void Push(const uint64_t ticks_count, F* pointer, void(F::*func)(Args...), Args... args) {
+            queue_.emplace(ticks_count, [func, pointer, args...](){(pointer->*func)(args...);});
+        }
+
+        template<typename F, typename... Args>
+        void Push(const uint64_t ticks_count, const F* pointer, void(F::*func)(Args...) const, Args... args) {
+            queue_.emplace(ticks_count, [func, pointer, args...](){(pointer->*func)(args...);});
         }
 
         void TryExecute(const uint64_t time_point);
