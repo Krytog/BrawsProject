0b1d664a81b660950c8df255d67d7c41901635b9
Ilya Parfenenko <yuliaparfenenko2015@gmail.com>
2023-01-25 22:11:34 +0300

    change Invoke and Push

--- Game/Core/DelayQueue.h
+++ Game/Core/DelayQueue.h
@@ -11,36 +11,26 @@ public:
     using TypeQueueTicks = std::pair<uint64_t, std::function<void()>>;
     static DelayQueue& GetInstance();
 
-    template <typename... Args, typename... Params>
-    void PushTime(const TimePoint& time_point, void (*func)(Args...), Params... args) {
-        queue_time_.Push(time_point, func, std::forward<Params>(args)...);
+    template <typename Callable, typename... Params>
+    void PushTime(const TimePoint& time_point, Callable&& callable, Params&&... params) {
+        queue_time_.Push(time_point, std::forward<Callable>(callable), std::forward<Params>(params)...);
     }
 
-    template <typename F, typename... Args, typename... Params>
-    void PushTime(const TimePoint& time_point, F* pointer, void (F::*func)(Args...), Params... args) {
-        queue_time_.Push(time_point, pointer, func, std::forward<Params>(args)...);
+    template <typename F, typename Callable, typename... Params>
+    void PushTime(const TimePoint& time_point, F* pointer, Callable&& callable, Params&&... params) {
+        queue_time_.Push(time_point, pointer, std::forward<Callable>(callable),
+                         std::forward<Params>(params)...);
     }
 
-    template <typename F, typename... Args, typename... Params>
-    void PushTime(const TimePoint& time_point, const F* pointer, void (F::*func)(Args...) const,
-                  Params... args) {
-        queue_time_.Push(time_point, pointer, func, std::forward<Params>(args)...);
+    template <typename Callable, typename... Params>
+    void PushTicks(const uint64_t ticks_count, Callable&& callable, Params&&... params) {
+        queue_ticks_.Push(ticks_count, std::forward<Callable>(callable), std::forward<Params>(params)...);
     }
 
-    template <typename... Args, typename... Params>
-    void PushTicks(const uint64_t ticks_count, void (*func)(Args...), Params... args) {
-        queue_ticks_.Push(ticks_count, func, std::forward<Params>(args)...);
-    }
-
-    template <typename F, typename... Args, typename... Params>
-    void PushTicks(const uint64_t ticks_count, F* pointer, void (F::*func)(Args...), Params... args) {
-        queue_ticks_.Push(ticks_count, pointer, func, std::forward<Params>(args)...);
-    }
-
-    template <typename F, typename... Args, typename... Params>
-    void PushTicks(const uint64_t ticks_count, const F* pointer, void (F::*func)(Args...) const,
-                   Params... args) {
-        queue_ticks_.Push(ticks_count, pointer, func, std::forward<Params>(args)...);
+    template <typename F, typename Callable, typename... Params>
+    void PushTicks(const uint64_t ticks_count, F* pointer, Callable&& callable, Params&&... params) {
+        queue_ticks_.Push(ticks_count, pointer, std::forward<Callable>(callable),
+                          std::forward<Params>(params)...);
     }
 
     void TryExecute(const TimePoint& time_point, const uint64_t ticks_count);
@@ -57,20 +47,24 @@ private:
             bool operator()(const TypeQueueTime& first, const TypeQueueTime& second);
         };
 
-        template <typename... Args, typename... Params>
-        void Push(const TimePoint& time_point, void (*func)(Args...), Params... args) {
-            queue_.emplace(time_point, [func, args...]() { func(args...); });
+        template <typename CallableReturn, typename... CallableArgs, typename... Params,
+                  std::enable_if_t<std::is_void_v<CallableReturn>, bool> = true>
+        void Push(const TimePoint& time_point, CallableReturn (*func)(CallableArgs...), Params&&... args) {
+            queue_.emplace(time_point, [func, args...] { func(args...); });
         }
 
-        template <typename F, typename... Args, typename... Params>
-        void Push(const TimePoint& time_point, F* pointer, void (F::*func)(Args...), Params... args) {
-            queue_.emplace(time_point, [func, pointer, args...]() { (pointer->*func)(args...); });
+        template <typename F, typename CallableReturn, typename... CallableArgs, typename... Params,
+                  std::enable_if_t<std::is_void_v<CallableReturn>, bool> = true>
+        void Push(const TimePoint& time_point, F* pointer, CallableReturn (F::*func)(CallableArgs...),
+                  Params&&... args) {
+            queue_.emplace(time_point, [func, pointer, args...] { (pointer->*func)(args...); });
         }
 
-        template <typename F, typename... Args, typename... Params>
-        void Push(const TimePoint& time_point, const F* pointer, void (F::*func)(Args...) const,
-                  Params... args) {
-            queue_.emplace(time_point, [func, pointer, args...]() { (pointer->*func)(args...); });
+        template <typename F, typename CallableReturn, typename... CallableArgs, typename... Params,
+                  std::enable_if_t<std::is_void_v<CallableReturn>, bool> = true>
+        void Push(const TimePoint& time_point, const F* pointer,
+                  CallableReturn (F::*func)(CallableArgs...) const, Params&&... args) {
+            queue_.emplace(time_point, [func, pointer, args...] { (pointer->*func)(args...); });
         }
 
         void TryExecute(const TimePoint& time_point);
@@ -88,19 +82,23 @@ private:
             bool operator()(const TypeQueueTicks& first, const TypeQueueTicks& second);
         };
 
-        template <typename... Args, typename... Params>
-        void Push(const uint64_t ticks_count, void (*func)(Args...), Params... args) {
+        template <typename CallableReturn, typename... CallableArgs, typename... Params,
+                  std::enable_if_t<std::is_void_v<CallableReturn>, bool> = true>
+        void Push(const uint64_t ticks_count, CallableReturn (*func)(CallableArgs...), Params&&... args) {
             queue_.emplace(ticks_count, [func, args...]() { func(args...); });
         }
 
-        template <typename F, typename... Args, typename... Params>
-        void Push(const uint64_t ticks_count, F* pointer, void (F::*func)(Args...), Params... args) {
+        template <typename F, typename CallableReturn, typename... CallableArgs, typename... Params,
+                  std::enable_if_t<std::is_void_v<CallableReturn>, bool> = true>
+        void Push(const uint64_t ticks_count, F* pointer, CallableReturn (F::*func)(CallableArgs...),
+                  Params&&... args) {
             queue_.emplace(ticks_count, [func, pointer, args...]() { (pointer->*func)(args...); });
         }
 
-        template <typename F, typename... Args, typename... Params>
-        void Push(const uint64_t ticks_count, const F* pointer, void (F::*func)(Args...) const,
-                  Params... args) {
+        template <typename F, typename CallableReturn, typename... CallableArgs, typename... Params,
+                  std::enable_if_t<std::is_void_v<CallableReturn>, bool> = true>
+        void Push(const uint64_t ticks_count, const F* pointer,
+                  CallableReturn (F::*func)(CallableArgs...) const, Params&&... args) {
             queue_.emplace(ticks_count, [func, pointer, args...]() { (pointer->*func)(args...); });
         }
 
--- Game/Core/Engine.h
+++ Game/Core/Engine.h
@@ -51,31 +51,23 @@ public:
     void ReadNewInput();
     InputSystem::InputTokensArray GetInput() const;
 
-    template <typename... Args>
-    void Invoke(const std::chrono::milliseconds& milliseconds, void (*func)(Args...), Args... args) {
-        delay_queue_.PushTime(std::chrono::steady_clock::now() + milliseconds, func, args...);
+    template <typename Callable, typename... Args>
+    void Invoke(const std::chrono::milliseconds& milliseconds, Callable&& cb, Args... args) {
+        delay_queue_.PushTime(std::chrono::steady_clock::now() + milliseconds, std::forward<Callable>(cb),
+                              std::forward<Args>(args)...);
     }
-    template <typename... Args>
-    void Invoke(const uint64_t ticks_count, void (*func)(Args...), Args... args) {
-        delay_queue_.PushTicks(ticks_count, func, args...);
+    template <typename Callable, typename... Args>
+    void Invoke(const uint64_t ticks_count, Callable&& cb, Args... args) {
+        delay_queue_.PushTicks(ticks_count, std::forward<Callable>(cb), std::forward<Args>(args)...);
     }
-    template <typename F, typename... Args>
-    void Invoke(const std::chrono::milliseconds& milliseconds, F* pointer, void (F::*func)(Args...),
-                Args... args) {
-        delay_queue_.PushTime(std::chrono::steady_clock::now() + milliseconds, pointer, func, args...);
+    template <typename F, typename Callable, typename... Args>
+    void Invoke(const std::chrono::milliseconds& milliseconds, F* pointer, Callable&& cb, Args... args) {
+        delay_queue_.PushTime(std::chrono::steady_clock::now() + milliseconds, pointer,
+                              std::forward<Callable>(cb), std::forward<Args>(args)...);
     }
-    template <typename F, typename... Args>
-    void Invoke(const uint64_t ticks_count, F* pointer, void (F::*func)(Args...), Args... args) {
-        delay_queue_.PushTicks(ticks_count, pointer, func, args...);
-    }
-    template <typename F, typename... Args>
-    void Invoke(const std::chrono::milliseconds& milliseconds, const F* pointer,
-                void (F::*func)(Args...) const, Args... args) {
-        delay_queue_.PushTime(std::chrono::steady_clock::now() + milliseconds, pointer, func, args...);
-    }
-    template <typename F, typename... Args>
-    void Invoke(const uint64_t ticks_count, const F* pointer, void (F::*func)(Args...) const, Args... args) {
-        delay_queue_.PushTicks(ticks_count, pointer, func, args...);
+    template <typename F, typename Callable, typename... Args>
+    void Invoke(const uint64_t ticks_count, F* pointer, Callable&& cb, Args... args) {
+        delay_queue_.PushTicks(ticks_count, pointer, std::forward<Callable>(cb), std::forward<Args>(args)...);
     }
 
     // temporary public, should be private in the future as it will be called in Update method of
