b51df90f246f951d3fbd5abce315bc304daf8be7
Matthew Krivda <makrivda@edu.hse.ru>
2023-04-04 17:02:26 +0300

    serialized v1

---
+++ Game/SwarmSystem/serializer.h
@@ -0,0 +1,28 @@
+#include <string>
+#include <cstring>
+
+class Serializer {
+public:
+    template <typename T>
+    static size_t Serialize(const T& obj, std::string& result) {
+        std::string bytes;
+        bytes.resize(sizeof(obj));
+
+        // little-big endian!?
+
+        std::memcpy(&bytes[0], &obj, sizeof(obj));
+        result = std::move(bytes);
+        return result.size();
+    }
+
+    template <typename T>
+    static size_t Deserialize(T& obj, const std::string& src) {
+        if (sizeof(obj) != src.size()) {
+            return 0;
+        }
+
+        // little-big endian!?
+        std::memcpy(&obj, src.data(), sizeof(obj));
+        return src.size();
+    }
+};
