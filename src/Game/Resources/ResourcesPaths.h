3e567e6db4890ca47ca5b58e07a9bb0dcb73defd
HazyMrf <yuliaparfenenko2015@gmail.com>
2023-04-28 21:13:37 +0300

    change the path to png files

--- src/Game/Resources/ResourcesPaths.h
+++ src/Game/Resources/ResourcesPaths.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define RES_PATH_MAPS_DEFAULT_BACKGROUND "../../src/Game/Resources/Maps/Default/map1_background.png"
-#define RES_PATH_CHARACTERS_DEFAULT "../../src/Game/Resources/Characters/Character1.png"
-#define RES_PATH_CHARACTERS_MAGE "../../src/Game/Resources/Characters/Character2.png"
+#define RES_PATH_MAPS_DEFAULT_BACKGROUND "../src/Game/Resources/Maps/Default/map1_background.png"
+#define RES_PATH_CHARACTERS_DEFAULT "../src/Game/Resources/Characters/Character1.png"
+#define RES_PATH_CHARACTERS_MAGE "../src/Game/Resources/Characters/Character2.png"
