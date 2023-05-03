f2784b012597f95d7d7a2a060a35e68088e91619
HazyMrf <yuliaparfenenko2015@gmail.com>
2023-05-03 22:25:50 +0300

    make BotInputSystem a singleton

--- src/BotAPI/BotInputSystem.h
+++ src/BotAPI/BotInputSystem.h
@@ -1,9 +1,10 @@
 #include <Core/InputSystem.h>
 
 class BotInputSystem : public InputSystem {
+public:
     void ReadNewInput() override;
 
-    BotInputSystem& InitInstance(const sf::Window&);
+    static BotInputSystem& InitInstance(const sf::Window&);
 private:
     explicit BotInputSystem(const sf::Window&);
     BotInputSystem(const BotInputSystem&) = delete;
