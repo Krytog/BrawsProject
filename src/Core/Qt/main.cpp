ed68177479e732e6cce764bdfd2464fda7de0795
trololo23 <egorbeketov37@gmail.com>
2023-05-04 23:00:18 +0300

    Fix paths in main.cpp

--- src/Core/Qt/main.cpp
+++ src/Core/Qt/main.cpp
@@ -20,13 +20,13 @@ int main(int argc, char *argv[])
     TMainWidget* main_window = new TMainWidget();
     main_window->show();
 
-    BasicSprite* image = new BasicSprite("soome.png", {10, -20}, 100, 100);
+    BasicSprite* image = new BasicSprite("../RecourcesForTesting/soome.png", {10, -20}, 100, 100);
     main_window->AddToRender(new PersistentObject(), image);
 
-    BasicSprite* back = new BasicSprite("back.jpeg", {0, 0}, 1000, 700, IVisibleObject::RenderLayers::BaseLayer);
+    BasicSprite* back = new BasicSprite("../RecourcesForTesting/back.jpeg", {0, 0}, 1000, 700, IVisibleObject::RenderLayers::BaseLayer);
     main_window->AddToRender(new PersistentObject(), back);
 
-    AnimatedSprite* guy = new AnimatedSprite("guuy.png", {-20, 0}, 300, 300, 25, 3, false);
+    AnimatedSprite* guy = new AnimatedSprite("../RecourcesForTesting/guuy.png", {-20, 0}, 300, 300, 25, 3, false);
     main_window->AddToRender(new PersistentObject(), guy);
     // guy->ChangeRenderLogic([guy](QPainter* painter){
         
